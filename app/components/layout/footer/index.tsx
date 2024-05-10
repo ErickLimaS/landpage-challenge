2579db7217b665568e92609deaf55c3e2276a0cb
Erick Lima <ericklima63@gmail.com>
2024-05-10 15:23:16 -0300

    style(logo imgs): Replace Logo Imgs
    Change for a better quality and good fit.

--- app/components/layout/footer/index.tsx
+++ app/components/layout/footer/index.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import React from 'react'
-import Logo from "@/public/assets/img/1.png" // placeholder img
+import Logo from "@/public/assets/img/logo3.jpeg" // logo2 svg couldnt be stretched with a good quality
 import Link from 'next/link'
 
 function Footer() {
     return (
-        <footer className='pt-6 pb-6 md:pt-8 md:pb-8 bg-footer flex items-center justify-center'>
+        <footer className='py-6 md:py-16 bg-footer flex items-center justify-center'>
 
-            <div className='max-w-36 w-full sm:max-w-30 '>
+            <div>
 
-                <Link href={'/'} className='block max-w-fit'>
+                <Link href={'/'}>
 
                     <Image src={Logo} alt={'Logo TESIS'} />
 
@@ -19,7 +19,6 @@ function Footer() {
 
         </footer>
     )
-
 }
 
 export default Footer
--- app/components/layout/header/index.tsx
+++ app/components/layout/header/index.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
-import Logo from "@/public/assets/img/1.png"
+import Logo from "@/public/assets/img/logo1.png"
 import Link from 'next/link'
 
 function Header() {
     return (
-        <header className='pt-6 pb-6 md:pt-8 md:pb-8 bg-offwhite flex md:items-center md:justify-center '>
+        <header className='flex md:items-center md:justify-center py-4 md:py-6 bg-offwhite'>
 
             <div className='pl-4 xl:pl-0 max-w-36 w-full sm:max-w-30 md:max-w-7xl'>
 
@@ -19,7 +19,6 @@ function Header() {
 
         </header>
     )
-
 }
 
 export default Header
